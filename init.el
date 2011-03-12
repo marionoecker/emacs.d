825e89d0db449385373378b9ec1fdc44019b95d2
marionoecker <mario.noecker@freiheit.com>
2011-03-12 13:14:43 -0800

    Edited init.el via GitHub

--- init.el
+++ init.el
@@ -1,18 +1,13 @@
-;; Using 1024x768
-;; see: http://www.emacswiki.org/emacs/GoodFonts
-;; sudo apt-get install xfonts-terminus  console-terminus
-(set-face-attribute 'default nil :font "-*-terminus-medium-r-*-*-*-140-75-75-*-*-iso8859-15")
-
 ;; see http://riddell.us/ClojureSwankLeiningenWithEmacsOnLinux.html
 ;; clojure-mode
-(add-to-list 'load-path "~/opt/clojure-mode")
+(add-to-list 'load-path "~/clojure/clojure-mode")
 (require 'clojure-mode)
 
 ;; slime
 (eval-after-load "slime" 
   '(progn (slime-setup '(slime-repl))))
 
-(add-to-list 'load-path "~/opt/slime")
+(add-to-list 'load-path "~/clojure/slime")
 (require 'slime)
 (slime-setup)
 
@@ -32,7 +27,7 @@
 ;; see https://github.com/technomancy/swank-clojure/blob/master/README.md
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
 
-(add-to-list 'load-path "~/opt/emacs-deps")
+(add-to-list 'load-path "~/clojure/emacs_deps")
 ; see http://clojure.roboloco.net/?p=979
 ;; wget http://mumble.net/~campbell/emacs/paredit.el
 (require 'paredit)
